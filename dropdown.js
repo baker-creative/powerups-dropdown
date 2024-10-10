521a4c97ebe00da50c57a81ffc50a635c13aecb5
Baker Creative <bakercreativedesign1@gmail.com>
2024-10-10 03:38:35 +0300

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -1,1 +1,101 @@
-(()=>{var u=document,r=(e,o=u)=>o.querySelector(e),i=(e,o=u)=>o.querySelectorAll(e);var w=(e,o)=>{let t=e==null?void 0:e.classList.toString(),n=new MutationObserver(s=>{for(let d of s)if(d.attributeName==="class"){let c=e==null?void 0:e.classList.toString();if(c!==t){o(n),t=c;break}}});return n.observe(e,{attributes:!0}),n};var a=e=>{var o=["mousedown","click","mouseup"];o.forEach(t=>e.dispatchEvent(new MouseEvent(t,{view:window,bubbles:!0,cancelable:!0,buttons:1})))};var l=e=>{var t,n;let o=((t=window.bc)==null?void 0:t.smoothscroll)&&((n=window.bc)==null?void 0:n.smoothscroll)instanceof Lenis;if(e){document.documentElement.style.overflow="hidden",document.body.style.overflow="hidden",o&&window.bc.smoothscroll.stop();return}document.documentElement.style.overflow="",document.body.style.overflow="",o&&window.bc.smoothscroll.start()};var p=i(".w-dropdown[data-bc-dropdown]"),m=i("[data-bc-dropdown-close]");m.forEach(e=>{e.addEventListener("click",()=>{let o=e.closest(".w-dropdown[data-bc-dropdown]"),t=r(".w-dropdown-toggle",o);t.classList.contains("w--open")&&a(t)})});document.addEventListener("keydown",e=>{e.key==="Escape"&&p.forEach(o=>{let t=r(".w-dropdown-toggle",o),n=()=>{let s=i(".w-dropdown[data-bc-dropdown]",o);return s.length?![...s].some(c=>r(".w-dropdown-toggle",c).classList.contains("w--open")):!0};if(t.classList.contains("w--open")){if(!n)return;a(t)}})});p.forEach(e=>{let o=r(".w-dropdown-toggle",e),t=e.hasAttribute("data-bc-scroll-lock");w(o,()=>{if(o.classList.contains("w--open")){t&&l(!0);return}t&&l(!1)})});})();
+(() => {
+  // bin/live-reload.js
+  new EventSource(`${"http://localhost:3133"}/esbuild`).addEventListener("change", () => location.reload());
+
+  // src/~utils/common.js
+  var doc = document;
+  var qs = (s, o = doc) => o.querySelector(s);
+  var qsa = (s, o = doc) => o.querySelectorAll(s);
+  var onClassChange = (node, callback) => {
+    let lastClassString = node == null ? void 0 : node.classList.toString();
+    const mutationObserver = new MutationObserver((mutationList) => {
+      for (const item of mutationList) {
+        if (item.attributeName === "class") {
+          const classString = node == null ? void 0 : node.classList.toString();
+          if (classString !== lastClassString) {
+            callback(mutationObserver);
+            lastClassString = classString;
+            break;
+          }
+        }
+      }
+    });
+    mutationObserver.observe(node, { attributes: true });
+    return mutationObserver;
+  };
+  var simulateClick = (element) => {
+    var mouseClickEvents = ["mousedown", "click", "mouseup"];
+    mouseClickEvents.forEach(
+      (mouseEventType) => element.dispatchEvent(new MouseEvent(mouseEventType, { view: window, bubbles: true, cancelable: true, buttons: 1 }))
+    );
+  };
+  var toggleScrollLock = (active) => {
+    var _a, _b;
+    const hasLenis = ((_a = window.bc) == null ? void 0 : _a.smoothscroll) && ((_b = window.bc) == null ? void 0 : _b.smoothscroll) instanceof Lenis;
+    if (active) {
+      document.documentElement.style.overflow = "hidden";
+      document.body.style.overflow = "hidden";
+      if (hasLenis)
+        window.bc.smoothscroll.stop();
+      return;
+    }
+    document.documentElement.style.overflow = "";
+    document.body.style.overflow = "";
+    if (hasLenis)
+      window.bc.smoothscroll.start();
+  };
+
+  // src/powerups-dropdown/dropdown.js
+  var dropdowns = qsa(".w-dropdown[data-bc-dropdown]");
+  var dropdownCloseButtons = qsa("[data-bc-dropdown-close]");
+  dropdownCloseButtons.forEach((el) => {
+    el.addEventListener("click", () => {
+      const dropdown = el.closest(".w-dropdown[data-bc-dropdown]");
+      const dropdownTrigger = qs(".w-dropdown-toggle", dropdown);
+      if (dropdownTrigger.classList.contains("w--open"))
+        simulateClick(dropdownTrigger);
+    });
+  });
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      dropdowns.forEach((dropdown) => {
+        const dropdownTrigger = qs(".w-dropdown-toggle", dropdown);
+        const canCloseDropdown = () => {
+          const childDropdowns = qsa(".w-dropdown[data-bc-dropdown]", dropdown);
+          if (!childDropdowns.length)
+            return true;
+          const anyChildDropdownOpen = [...childDropdowns].some((el) => qs(".w-dropdown-toggle", el).classList.contains("w--open"));
+          if (anyChildDropdownOpen)
+            return false;
+          return true;
+        };
+        if (dropdownTrigger.classList.contains("w--open")) {
+          if (!canCloseDropdown)
+            return;
+          simulateClick(dropdownTrigger);
+        }
+      });
+    }
+  });
+  dropdowns.forEach((dropdown) => {
+    const dropdownTrigger = qs(".w-dropdown-toggle", dropdown);
+    const dropdownContent = qs(".w-dropdown-list", dropdown);
+    const lockScroll = dropdown.hasAttribute("data-bc-scroll-lock");
+    onClassChange(dropdownTrigger, () => {
+      var _a;
+      if (dropdownTrigger.classList.contains("w--open")) {
+        const hasSlider = dropdownContent.querySelector("[data-bc-slider]");
+        if (hasSlider && ((_a = window.bc) == null ? void 0 : _a.slider)) {
+          qsa("[data-bc-slider-track]", marker).forEach((track) => {
+            window.bc.slider.data(track).resize();
+          });
+        }
+        if (lockScroll)
+          toggleScrollLock(true);
+        return;
+      }
+      if (lockScroll)
+        toggleScrollLock(false);
+    });
+  });
+})();
